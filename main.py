37df08738f8046dd7346ca8b7f27bfbbe314ba86
Francesco Di Fraia <francesco@difraia.com>
2025-05-14 10:59:27 +0200

    v2

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from re import search
 from tkinter import *
 from tkinter import messagebox
 from random import choice, shuffle
 import json as js
-
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def psw_generator():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
